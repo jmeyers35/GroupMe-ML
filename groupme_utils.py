80cd99db18af28cbdd6f2139eac580b58636a702
Jacob Meyers <jmeyers35@gatech.edu>
2018-10-05 20:55:03 -0400

    partially implemented get_messages_by_user

--- groupme_utils.py
+++ groupme_utils.py
@@ -15,7 +15,9 @@ Returns all messages sent by user identified by user_id in group identified by g
 def get_messages_by_user(group_id, user_id):
     msgs = get_messages(group_id)
 
-    return filter(lambda x: x['sender_id'] == user_id)
+    res =  list(filter(lambda x: x['sender_id'] == user_id, msgs))
+    
+    return (res, len(res))
 
 """
 Returns a list of all the messages of a group identified by parameter id.
@@ -53,7 +55,7 @@ def get_messages(group_id):
 
 
 """
-Returns a list of dicts containing the groups the user corresponding to t   he access token is in.
+Returns a list of dicts containing the groups the user corresponding to the access token is in.
 
 Returns list<dict>
 """
@@ -70,7 +72,7 @@ def get_groups():
 Returns a list of tuples containing the name and id of all the groups the user corresponding to the access
 token is in.
 """
-def get_group_names_ids():
+def get_group_ids():
     url = BASE_URL + "/groups"
     url += "?token=" + GROUPME_TOKEN
 
@@ -84,4 +86,24 @@ def get_group_names_ids():
         tups.append((group_dict['name'], group_dict['id']))
 
     return tups
-    
+
+def get_group_by_id(group_id):
+    groups = get_groups()
+
+    try:
+        return next(x for x in groups if x['id'] == group_id)
+
+    except StopIteration:
+        print("No group found with id " + str(group_id))
+        
+        return None
+
+def get_users_by_group(group_id):
+    group = get_group_by_id(group_id)
+
+    members = []
+
+    for member in group['members']:
+        members.append((member['name'], member['id']))
+
+    return members
